8e76793be9d74337d81e8bd28eab9fe2677ea979
Dan King <king.daniel.thomas@gmail.com>
2024-06-08 16:09:42 +0700

    Import my code from LL project

---
+++ LinkedList.js
@@ -0,0 +1,52 @@
+class LinkedList {
+  constructor(){
+    this.head = null;
+    this.length = 0;
+  }
+
+  prepend (value) {
+    const newNode = new Node(value, this.head);
+    this.head = newNode;
+    this.length += 1;
+  }
+
+  append (value) {
+    // if list is empty, simply assign a newNode to this.head
+    if (this.head == null){
+      this.prepend(value)
+    } else {
+      // use a temp copy of head as a non-destructive way to traverse by continually reassigning itself to the next one.
+      let temp = this.head;
+      while (temp.nextNode != null) {
+        temp = temp.nextNode; // Notice we don't reassign temp.value - but the whole object
+        // This structure indicates that each node is actually nested inside its parent node
+      }
+      temp.nextNode = new Node(value);
+      this.length += 1;
+    }
+  }
+
+  print(){
+    let niceArray = [];
+    let niceString= '';
+    let temp = this.head;
+    while (temp){ // while temp is truthy (to include the final value)
+      niceArray.push(temp.value)
+      temp = temp.nextNode;
+    }
+    niceArray.forEach((item) => {
+      niceString += ` (${item.toString()}) ->`;
+    })
+    niceString += ' null'
+    console.log(niceString)
+  }
+}
+
+class Node {
+  constructor (value, nextNode) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+module.exports = LinkedList
